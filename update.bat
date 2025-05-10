eb18fbdbbd2c6f236a33b4f8a4debf302c648163
25garcial <25garcial@camraiders.com>
2025-05-10 11:40:37 -0500

    added update script

---
+++ update.bat
@@ -0,0 +1,19 @@
+
+@echo off
+setlocal
+
+:: Set the URL and destination path
+set "URL=https://github.com/Logan-Garcia-inc/sound-board/raw/refs/heads/main/main.py"
+set "DEST=%~dp0main.py"
+
+echo Downloading file from %URL%...
+curl -L -o "%DEST%" "%URL%"
+
+if %ERRORLEVEL% equ 0 (
+    echo File successfully downloaded to %DEST%.
+) else (
+    echo Failed to download file.
+)
+
+endlocal
+pause
